dddea5c715824d6668393494a94a6adeecc9c44d
ikseth <ignaciogarciahoyos@gmail.com>
2025-12-11 15:08:43 +0100

    Update users.php

--- my_ia_site/config/users.php
+++ my_ia_site/config/users.php
@@ -1,10 +1,5 @@
 <?php
 return [
-    'admin' => ['pass' => '$2y$12$mRD/O5acXkXEPN2c9Bdef.eEGDqXfouwLxl87aDdUjyi1T9lZGmDK', 'rol' => 'admin', 'fecha_nacimiento' => '1900-01-01'],
-    'usuario' => ['pass' => '$2y$12$aDa0Op3f4VGTPp9aeZy9O.AEEUT47pu5zSY9Mpk8yGRlJLu58t8kO', 'rol' => 'user', 'fecha_nacimiento' => '2017-01-01'],
-    'Martin' => ['pass' => '$2y$12$qz..HtKhs5U888zhUlqKTeT1oAQJ3FbbuTxNvRI5W8No30NaCGpdG', 'rol' => 'user', 'fecha_nacimiento' => '2017-12-17'],
-    'Blanca' => ['pass' => '$2y$12$RtxD9blwhc/nROUkticTa.BVUR0CzBGGmaJxjU/JAMTrlU8fO6dpC', 'rol' => 'user', 'fecha_nacimiento' => '2013-09-28'],
-    'Cristina' => ['pass' => '$2y$12$X/dkjhwCYOC1EkkgneLyOeAw95wJ1GGRm5Lln/4nNeByON5PGy55i', 'rol' => 'user', 'fecha_nacimiento' => '1980-07-12'],
-    'ignacio' => ['pass' => '$2y$12$cY7Zqgd8Zq8uF3aV5EJQcOnT76mcyWi7t7YLMcbaRQiJROh8l.1X.', 'rol' => 'user', 'fecha_nacimiento' => '1949-09-25'],
-    'ignacio.garcia' => ['pass' => '$2y$12$kAlZ4VIgx/ixkLL2Hb0ED.DQJ5FPn94ap/A7//zr.0aZQGW6FVNV2', 'rol' => 'admin', 'fecha_nacimiento' => '1974-03-14'],
+    'admin' => ['pass' => '', 'rol' => 'admin', 'fecha_nacimiento' => '1900-01-01'],
+    'usuario' => ['pass' => '', 'rol' => 'user', 'fecha_nacimiento' => '2017-01-01'],
 ];
